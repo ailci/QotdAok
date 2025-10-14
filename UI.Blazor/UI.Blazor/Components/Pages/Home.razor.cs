b4135acbf864b134fdb3a3e59761d23a0b440776
ailci <ali.ilci@gmx.de>
2025-10-14 10:13:17 +0200

    QotdVm

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -1,5 +1,31 @@
+using Application.ViewModels.Qotd;
+using Microsoft.AspNetCore.Components;
+
 namespace UI.Blazor.Components.Pages;
 public partial class Home
 {
+    [Inject]
+    public ILogger<Home> Logger { get; set; } = null!;
 
+    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Logger.LogInformation("OnInitialized aufgerufen...");
+
+        QotdViewModel = new QuoteOfTheDayViewModel
+        {
+            QuoteText = "Larum lierum Löffelstiel",
+            AuthorName = "Ich",
+            AuthorDescription = "Dozent",
+            AuthorBirthDate = new DateOnly(1978, 07, 13)
+        };
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        Logger.LogInformation("OnInitializedAsync aufgerufen...");
+
+        return Task.CompletedTask;
+    }
 }
