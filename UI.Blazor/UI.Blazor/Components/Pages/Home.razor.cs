27fe22810e5a4daff6989fe9379e5e12103f535e
ailci <ali.ilci@gmx.de>
2025-10-15 12:16:24 +0200

    Enable state persistence and add BlazorInfo component
    Added `@rendermode` directive in `Home.razor` for interactive server-side rendering with `InteractiveServerRenderMode`. Updated `Home.razor.cs` to implement `IDisposable` and manage state persistence using `PersistentComponentState`. Introduced logic to restore or fetch `QuoteOfTheDayViewModel` in `OnInitializedAsync` and added a `PersistData` method for state persistence. Registered and disposed of `PersistingComponentStateSubscription`.
    
    Added a new `<BlazorInfo />` component to display Blazor renderer details. Removed an unused `@code` block from `QuoteOfTheDay.razor`. Commented out an alternative `OnAfterRenderAsync` implementation for fetching data.

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -6,10 +6,12 @@ using Persistence;
 
 namespace UI.Blazor.Components.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject] public ILogger<Home> Logger { get; set; } = null!;
     [Inject] public IServiceManager ServiceManager { get; set; } = null!;
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = null!;
+    private PersistingComponentStateSubscription _persistingComponentStateSubscription;
 
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
@@ -17,6 +19,33 @@ public partial class Home
     {
         Logger.LogInformation("OnInitializedAsync aufgerufen...");
 
-        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        //3.Lösung
+        _persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistData);
+        if (!ApplicationState.TryTakeFromJson<QuoteOfTheDayViewModel>(nameof(QotdViewModel), out var restoredData))
+        {
+            QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        }
+        else //gefunden
+        {
+            QotdViewModel = restoredData;
+        }
     }
+
+    private Task PersistData()
+    {
+        ApplicationState.PersistAsJson(nameof(QotdViewModel), QotdViewModel);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() => _persistingComponentStateSubscription.Dispose();
+
+    // 2.Variante
+    //protected override async Task OnAfterRenderAsync(bool firstRender)
+    //{
+    //    if (firstRender)
+    //    {
+    //        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+    //        StateHasChanged();
+    //    }
+    //}
 }
