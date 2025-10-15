2c1e3bd85d4af517ca1e6a871fe4b23df82e7693
ailci <ali.ilci@gmx.de>
2025-10-15 14:31:35 +0200

    Authors

---
+++ Application/ViewModels/Author/AuthorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.Author;
+
+public class AuthorViewModel
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public byte[]? Photo { get; set; }
+    public string? PhotoMimeType { get; set; }
+}
---
+++ UI.Blazor/UI.Blazor/Services/AuthorService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Contracts.Services;
+using Application.ViewModels.Author;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace UI.Blazor.Services;
+
+public class AuthorService(ILogger<AuthorService> logger, IDbContextFactory<QotdContext> contextFactory) : IAuthorService
+{
+    public async Task<IEnumerable<AuthorViewModel>> GetAuthorsAsync()
+    {
+        logger.LogInformation($"{nameof(GetAuthorsAsync)} aufgerufen...");
+
+        await using var context = await contextFactory.CreateDbContextAsync();
+    }
+}
