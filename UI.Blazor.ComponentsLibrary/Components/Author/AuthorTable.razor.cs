217e4f9ebeff4956b9ba08a776c1644010d6de0f
ailci <ali.ilci@gmx.de>
2025-10-16 13:57:22 +0200

    EventCallback

--- UI.Blazor.ComponentsLibrary/Components/Author/AuthorTable.razor.cs
+++ UI.Blazor.ComponentsLibrary/Components/Author/AuthorTable.razor.cs
@@ -1,9 +1,27 @@
 using Application.ViewModels.Author;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 
 namespace UI.Blazor.ComponentsLibrary.Components.Author;
 public partial class AuthorTable
 {
-    [Parameter]
-    public IEnumerable<AuthorViewModel>? AuthorViewModels { get; set; }
+    [Inject] public ILogger<AuthorTable> Logger { get; set; } = null!;
+    [Inject] public IJSRuntime JsRuntime { get; set; } = null!;
+    [Parameter] public EventCallback<Guid> OnAuthorDelete { get; set; }
+    [Parameter] public IEnumerable<AuthorViewModel>? AuthorViewModels { get; set; }
+    private Guid _authorIdToDelete;
+
+    private async Task ShowConfirmationDialog(AuthorViewModel authorVm)
+    {
+        Logger.LogInformation($"Author {authorVm?.Name} zum Löschen ausgewählt");
+
+        _authorIdToDelete = authorVm.Id;
+
+        //1. Version
+        if (await JsRuntime.InvokeAsync<bool>("confirm", $"Wollen Sie wirklich den Author {authorVm?.Name} löschen?"))
+        {
+            await OnAuthorDelete.InvokeAsync(_authorIdToDelete);
+        }
+    }
 }
