3a32626bdae5c37c9325f3c2f7afaaab38d71b97
ailci <ali.ilci@gmx.de>
2025-10-17 14:04:34 +0200

    logger

--- UI.Blazor/UI.Blazor.Client/Pages/HomeWasm.razor.cs
+++ UI.Blazor/UI.Blazor.Client/Pages/HomeWasm.razor.cs
@@ -1,7 +1,9 @@
 using Application.ViewModels.Qotd;
+using Microsoft.AspNetCore.Components;
 
 namespace UI.Blazor.Client.Pages;
 public partial class HomeWasm
 {
+    [Inject] public ILogger<HomeWasm> Logger { get; set; } = null!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 }
